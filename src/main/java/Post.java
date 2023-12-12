267cc782119f17ffc5c9e6b78fb962ebee524ae4
MihhFor~ <fr.mikle@ya.ru>
2023-12-12 12:27:03 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String phone;
+    public String passport;
+    public Boolean subscription;
+    public FormDate birthday;
+
+}
